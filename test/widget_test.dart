3d588aab050ebf576f9c8444990bff39f7d668be
Jay-Anne Gonzalez <72730422+msjgnzlzx@users.noreply.github.com>
2025-04-07 03:37:22 +0800

    Add files via upload

---
+++ test/widget_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter App'),
+        ),
+        body: const Center(
+          child: Text('0'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
